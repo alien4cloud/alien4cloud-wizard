0e231219a150ec8eb010115bcb8e95760e9e96db
Xavier DEGENNE <xavier.degenne@gmail.com>
2019-07-15 16:55:30 +0200

    Add FlexLout to ApplicationDashboard

--- alien4cloud-wizard-ui/src/main/webapp/app/features/application-dashboard/application-dashboard.module.ts
+++ alien4cloud-wizard-ui/src/main/webapp/app/features/application-dashboard/application-dashboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FlexLayoutModule } from '@angular/flex-layout';
 import {A4cMaterialModule} from '@app/shared';
 
 import { ApplicationDashboardRoutingModule } from './application-dashboard-routing.module';
@@ -11,6 +12,7 @@ import { ApplicationListComponent } from './application-list/application-list.co
   declarations: [ApplicationListComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     ApplicationDashboardRoutingModule,
     A4cMaterialModule,
     FormsModule,
