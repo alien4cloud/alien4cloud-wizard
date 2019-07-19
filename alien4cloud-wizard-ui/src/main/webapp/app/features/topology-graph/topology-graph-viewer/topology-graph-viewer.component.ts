ee97d08e237fe3a3d7f8720c6e43dd7af409be14
Xavier DEGENNE <xavier.degenne@gmail.com>
2019-07-19 15:54:23 +0200

    few fix onto graph preview

--- alien4cloud-wizard-ui/src/main/webapp/app/features/topology-graph/topology-graph-viewer/topology-graph-viewer.component.ts
+++ alien4cloud-wizard-ui/src/main/webapp/app/features/topology-graph/topology-graph-viewer/topology-graph-viewer.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {TopologyGraph} from "@app/core";
 import {Subject} from "rxjs";
+import * as _ from "lodash";
 
 @Component({
   selector: 'w4c-topology-graph-viewer',
@@ -8,6 +9,9 @@ import {Subject} from "rxjs";
   styleUrls: ['./topology-graph-viewer.component.css']
 })
 export class TopologyGraphViewerComponent implements OnInit {
+
+  // make lodash usable from template
+  private lodash = _;
 
   zoomToFit$: Subject<boolean> = new Subject();
   center$: Subject<boolean> = new Subject();
