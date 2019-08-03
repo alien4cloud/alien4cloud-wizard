af83ee85358fbf60a8bf562ca494dce8c7fd6cf2
Xavier DEGENNE <xavier.degenne@gmail.com>
2019-08-03 18:19:39 +0200

    a bit more cosmetic onto home

--- alien4cloud-wizard-ui/src/main/webapp/app/shared/layout/home.component.ts
+++ alien4cloud-wizard-ui/src/main/webapp/app/shared/layout/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     // this list will probably partially come from backend
     let dashboard = new Feature(
-      "Applications dashboard",
+      "Dashboard",
       "List all applications, view deployment status, view meta-data, view applications' modules ...",
       "dashboard",
       "/application-dashboard",
@@ -24,25 +24,22 @@ export class HomeComponent implements OnInit {
     );
     let wizard = new Feature(
       "Application Wizard",
-      "Create a new application from a template, step by step",
+      "Create a new application from a template, step by step.",
       "create_new_folder",
       "/new-wizard",
       true
     );
     let modules = new Feature(
       "Catalog",
-      "Browse platform components catalog and display details about components",
+      "Browse platform components catalog and display details about components.",
       "view_module",
       "/catalog",
       false
     );
 
-    // FIXME: just to figure out what it will look like with more feature
-    for (var _i = 0; _i < 3; _i++) {
-      this.features.push(dashboard);
-      this.features.push(wizard);
-      this.features.push(modules);
-    }
+    this.features.push(dashboard);
+    this.features.push(wizard);
+    this.features.push(modules);
 
   }
 
