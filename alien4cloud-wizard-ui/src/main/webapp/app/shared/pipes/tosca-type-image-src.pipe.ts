bef0c0513612c2efb4b7125dc2b9327864aed747
Xavier DEGENNE <xavier.degenne@gmail.com>
2019-08-20 14:38:32 +0200

    [ALIEN-3230] remove /api in image urls

--- alien4cloud-wizard-ui/src/main/webapp/app/shared/pipes/tosca-type-image-src.pipe.ts
+++ alien4cloud-wizard-ui/src/main/webapp/app/shared/pipes/tosca-type-image-src.pipe.ts
@@ -1,6 +1,7 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import {HasTags, NodeType} from '@app/core';
+import {Pipe, PipeTransform} from '@angular/core';
+import {HasTags} from '@app/core';
 import * as _ from 'lodash';
+import {environment} from '../../../environments/environment';
 
 /**
  * Returns the image url of a given NodeType by exploring it's tags.
@@ -12,7 +13,7 @@ export class ToscaTypeImageSrcPipe implements PipeTransform {
 
   transform(value: HasTags): string {
     let tagValue = _.get(_.find(value.tags, {name:'icon'}), 'value');
-    return `api/img?id=${tagValue}&quality=QUALITY_64`;
+    return environment.urlPrefix + `/img?id=${tagValue}&quality=QUALITY_64`;
   }
 
 }
