7f619988c4e960be57c4b716041c2470d3cc427c
Eusebe Kanani <knnezb@yahoo.fr>
2019-09-05 16:49:59 +0200

    [some-debugs]  Tabs label  customize , test ViewEncapsulation

--- alien4cloud-wizard-ui/src/main/webapp/app/shared/components/topology-overview/topology-overview.component.ts
+++ alien4cloud-wizard-ui/src/main/webapp/app/shared/components/topology-overview/topology-overview.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {Router} from '@angular/router';
 import {TopologyOverview} from "@app/core";
 
 @Component({
   selector: 'w4c-topology-overview',
   templateUrl: './topology-overview.component.html',
-  styleUrls: ['./topology-overview.component.scss']
+  styleUrls: ['./topology-overview.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TopologyOverviewComponent {
 
