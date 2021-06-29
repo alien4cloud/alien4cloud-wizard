1b225a5b9f96ce8bc139726747810d229f1d3a51
Xavier DEGENNE <>
2021-06-29 14:19:39 +0200

    [ALIEN-3679] move redirection feature in validation componnent (so fix redirect regression)

--- alien4cloud-wizard-ui/projects/wizard4cloud-ui/src/app/features/application-wizard/wizard-forms/deployment-validation/deployment-validation.component.ts
+++ alien4cloud-wizard-ui/projects/wizard4cloud-ui/src/app/features/application-wizard/wizard-forms/deployment-validation/deployment-validation.component.ts
@@ -14,9 +14,7 @@ import {SettingsService} from "@alien4cloud/wizard4cloud-commons";
 export class DeploymentValidationComponent extends WizardFormComponent implements OnInit {
 
   constructor(
-    protected fsm: AppplicationWizardMachineService,
-    private settingsService: SettingsService,
-    @Inject(DOCUMENT) private document: Document
+    protected fsm: AppplicationWizardMachineService
   ) { super(fsm); }
 
   deployApp() {
@@ -24,39 +22,6 @@ export class DeploymentValidationComponent extends WizardFormComponent implement
   }
 
   ngOnInit() {
-    const redirectInNewTab = this.settingsService.getSetting(SettingsService.REDIRECT_IN_NEW_TAB) == 'true';
-    console.log("taskList: ", this.fsmContext.deploymentTopologyDTO.validation.taskList);
-    if (this.fsmContext.deploymentTopologyDTO.validation.taskList) {
-      this.fsmContext.deploymentTopologyDTO.validation.taskList.filter(value => {
-        if (value.code == TaskCode.REDIRECTION_REQUIRED) {
-          console.log("Redirection detected: ", value);
-          console.log("Current url: ", window.location.href);
-          const task = <RedirectionTask>value;
-          let url = task.url;
-          if (task.backUrlParam) {
-            let regexp = /(.*)(new-wizard|app-wizard).*/g;
-            let groups = regexp.exec(window.location.href);
-            // We need to build a back URL regarding curent URL
-            // If we are in a new wizard, we will route to the app wizard (as if we were passed by dashboard)
-            let backUrl = groups[1] + "app-wizard/" + this.fsmContext.application.id + "/" + this.fsmContext.environment.id;
-            console.log("back url: ", backUrl);
-            if (url.indexOf("?") > -1 ) {
-              // URL has already params
-              url = url + "&" + task.backUrlParam + "=" + encodeURIComponent(backUrl);
-            } else {
-              // URL don't have params
-              url = url + "?" + task.backUrlParam + "=" + encodeURIComponent(backUrl);
-            }
-          }
-          if (redirectInNewTab) {
-            window.open(url, "_blank");
-          } else {
-            this.document.location.href = url;
-          }
-        }
-      })
-    }
-
   }
 
 }
--- alien4cloud-wizard-ui/projects/wizard4cloud-ui/src/app/shared/components/deployment-validation-tasks/deployment-validation.component.ts
+++ alien4cloud-wizard-ui/projects/wizard4cloud-ui/src/app/shared/components/deployment-validation-tasks/deployment-validation.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {PropertyDefinition} from "@alien4cloud/wizard4cloud-commons";
-import {TopologyValidationResult} from "@app/core/models";
+import {Component, Inject, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {PropertyDefinition, SettingsService} from "@alien4cloud/wizard4cloud-commons";
+import {RedirectionTask, TaskCode, TopologyValidationResult} from "@app/core/models";
 import {animate, state, style, transition, trigger} from "@angular/animations";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'w4c-deployment-validation-tasks',
@@ -24,11 +25,49 @@ export class DeploymentValidationComponent implements OnInit, OnChanges {
 
   @Input() topologyValidationResult: TopologyValidationResult;
 
+  @Input() applicationId: string;
+  @Input() environmentId: string;
+
   displayDetails: boolean = false;
 
-  constructor() { }
+  constructor(
+    private settingsService: SettingsService,
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
   ngOnInit() {
+    const redirectInNewTab = this.settingsService.getSetting(SettingsService.REDIRECT_IN_NEW_TAB) == 'true';
+    console.log("taskList: ", this.topologyValidationResult.taskList);
+    if (this.topologyValidationResult.taskList) {
+      this.topologyValidationResult.taskList.filter(value => {
+        if (value.code == TaskCode.REDIRECTION_REQUIRED) {
+          console.log("Redirection detected: ", value);
+          console.log("Current url: ", window.location.href);
+          const task = <RedirectionTask>value;
+          let url = task.url;
+          if (task.backUrlParam) {
+            let regexp = /(.*)(new-wizard|app-wizard).*/g;
+            let groups = regexp.exec(window.location.href);
+            // We need to build a back URL regarding curent URL
+            // If we are in a new wizard, we will route to the app wizard (as if we were passed by dashboard)
+            let backUrl = groups[1] + "app-wizard/" + this.applicationId + "/" + this.environmentId;
+            console.log("back url: ", backUrl);
+            if (url.indexOf("?") > -1 ) {
+              // URL has already params
+              url = url + "&" + task.backUrlParam + "=" + encodeURIComponent(backUrl);
+            } else {
+              // URL don't have params
+              url = url + "?" + task.backUrlParam + "=" + encodeURIComponent(backUrl);
+            }
+          }
+          if (redirectInNewTab) {
+            window.open(url, "_blank");
+          } else {
+            this.document.location.href = url;
+          }
+        }
+      })
+    }
   }
 
   onSwitch(){
